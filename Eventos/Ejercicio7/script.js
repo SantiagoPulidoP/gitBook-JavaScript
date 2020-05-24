27dd85f5ad098fdfee3172ff6e576030433ad5c2
Santiago Pulido <santiagopulidopelaez@gmail.com>
2020-05-24 06:25:54 -0500

    FreeCodeCamp JavaScript Certification

--- Eventos/Ejercicio7/script.js
+++ Eventos/Ejercicio7/script.js
@@ -4,13 +4,18 @@ const eternalSunshineOfTheSM = "Eternal sunshine of the spotless mind";
 const blueVelvet = "Blue velvet";
 const split = "Split";
 
+var t ;
 
 const changeText = (e)=>{
+	
+	
     let content = document.querySelector(".list");
     content.innerHTML = `<li class='inception'>${inception}</li><li class='butterfly'>${theButterFlyEffect}</li><li class='eternal'>${eternalSunshineOfTheSM}</li><li class='blue'>${blueVelvet}</li><li class='split'>${split}</li>`;
 
     content.addEventListener('click',(e)=>{
-        console.dir(e.target);
+	
+        console.dir(e.target.innerHTML);
+	  
     });
    
 
